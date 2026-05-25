6da3e441f9b6f85e9501dd93567c61ee03fa3e31
Caio Alves <136919293+EngCaio16@users.noreply.github.com>
2026-05-25 10:41:38 -0300

    Update quiz.js

--- api/quiz.js
+++ api/quiz.js
@@ -1,13 +1,11 @@
-import perguntas_quiz from '../political_quiz.js';
+const perguntas_quiz = require('../political_quiz.js');
 
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
+  if (req.method === 'OPTIONS') return res.status(200).end();
 
   if (req.method === 'GET') {
     return res.status(200).json({
@@ -17,4 +15,4 @@ export default function handler(req, res) {
   }
 
   return res.status(405).json({ error: 'Método não permitido.' });
-}
+};
